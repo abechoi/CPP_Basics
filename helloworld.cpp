10647f1f7e997580f96ab32c5641f5817354a057
abechoi <abraham.choi@icloud.com>
2019-08-12 18:09:58 -0700

    Hello World - Back to basics

---
+++ helloworld.cpp
@@ -0,0 +1,15 @@
+/*
+	Hello World:
+	1. Time to learn C++ the right way.
+
+	Only one path left. The one that leads to success.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	cout << "Hello World!\n";
+
+	return 0;
+}
